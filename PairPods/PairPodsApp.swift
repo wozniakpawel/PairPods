3add31db7c44c2cfb608d8f346822c7d4bd37b00
wozniakpawel <rax1994@gmail.com>
2024-03-02 13:50:25 +0800

    Initial Commit

---
+++ PairPods/PairPodsApp.swift
@@ -0,0 +1,17 @@
+//
+//  PairPodsApp.swift
+//  PairPods
+//
+//  Created by Pawel Wozniak on 02/03/2024.
+//
+
+import SwiftUI
+
+@main
+struct PairPodsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
