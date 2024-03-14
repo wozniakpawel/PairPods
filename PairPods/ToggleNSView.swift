701137b4295d0c90a10c278fc1034339492da878
wozniakpawel <rax1994@gmail.com>
2024-03-14 22:50:23 +0800

    clciking anywhere toggles

--- PairPods/ToggleNSView.swift
+++ PairPods/ToggleNSView.swift
@@ -11,10 +11,12 @@ class ToggleNSView: NSView {
     var toggleSwitch = NSSwitch(frame: .zero)
     var label = NSTextField()
     var onToggle: ((Bool) -> Void)?
+    private var trackingArea: NSTrackingArea?
 
     override init(frame frameRect: NSRect) {
         super.init(frame: frameRect)
         setupViews()
+        setupTrackingArea()
     }
     
     required init?(coder: NSCoder) {
@@ -55,5 +57,54 @@ class ToggleNSView: NSView {
     @objc func toggleAudioSharing(_ sender: NSSwitch) {
         onToggle?(sender.state == .on)
     }
+    
+    @objc func toggleSwitchChanged(_ sender: NSSwitch) {
+          // Instead of directly calling onToggle, change the state
+          // This allows the entire view to control the state change
+          isSharingAudio = sender.state == .on
+      }
+
+      var isSharingAudio: Bool {
+          get { toggleSwitch.state == .on }
+          set {
+              toggleSwitch.state = newValue ? .on : .off
+              onToggle?(newValue)
+          }
+      }
+      
+      private func setupTrackingArea() {
+          if let existingTrackingArea = trackingArea {
+              removeTrackingArea(existingTrackingArea)
+          }
+          
+          trackingArea = NSTrackingArea(rect: bounds, options: [.activeAlways, .mouseEnteredAndExited], owner: self, userInfo: nil)
+          if let newTrackingArea = trackingArea {
+              addTrackingArea(newTrackingArea)
+          }
+      }
+      
+      override func mouseEntered(with event: NSEvent) {
+          super.mouseEntered(with: event)
+          // Highlight the view by changing the background color or other visual cues
+      }
+
+      override func mouseExited(with event: NSEvent) {
+          super.mouseExited(with: event)
+          // Remove highlight
+      }
+      
+      override func updateTrackingAreas() {
+          super.updateTrackingAreas()
+          setupTrackingArea()
+      }
+
+      override func mouseDown(with event: NSEvent) {
+          isSharingAudio.toggle()
+      }
+      
+      override func draw(_ dirtyRect: NSRect) {
+          super.draw(dirtyRect)
+          // Drawing code for custom highlight or other styles if needed
+      }
     
 }
