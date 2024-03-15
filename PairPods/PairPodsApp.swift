bd5eef172232cb750c5747e89a914731dfc8046d
wozniakpawel <rax1994@gmail.com>
2024-03-15 22:50:23 +0800

    okay really simplfy the shit out of it

--- PairPods/PairPodsApp.swift
+++ PairPods/PairPodsApp.swift
@@ -9,11 +9,34 @@ import SwiftUI
 
 @main
 struct PairPodsApp: App {
-    @NSApplicationDelegateAdaptor(AppDelegate.self) var appDelegate
-
+    @StateObject private var viewModel = AudioSharingViewModel()
+    
     var body: some Scene {
-        Settings {
-            Text("PairPods Settings")
+        
+        MenuBarExtra {
+            Toggle(isOn: $viewModel.isSharingAudio) {
+                Text("Share Audio")
+            }.keyboardShortcut("s")
+
+            Divider()
+
+            Button("About") {
+                DispatchQueue.main.async {
+                    let alert = NSAlert()
+                    alert.messageText = "PairPods\nVersion: \(Bundle.main.object(forInfoDictionaryKey: "CFBundleShortVersionString") as? String ?? "Unknown")\nVertex Forge © \(Calendar.current.component(.year, from: Date()))"
+                    alert.addButton(withTitle: "OK")
+                    alert.runModal()
+                }
+            }.keyboardShortcut("a")
+            
+            Button("Quit") {
+                viewModel.isSharingAudio = false
+                NSApplication.shared.terminate(nil)
+            }.keyboardShortcut("q")
+            
+        } label: {
+            Image(systemName: viewModel.isSharingAudio ? "airpodspro.chargingcase.wireless.radiowaves.left.and.right.fill" : "airpodspro.chargingcase.wireless.fill")
+                .labelStyle(IconOnlyLabelStyle())
         }
     }
 }
