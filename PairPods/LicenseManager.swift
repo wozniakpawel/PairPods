e7374ec4c7d7a388bd198ff0cbc8dacad4e4ecbe
Pawel Wozniak <rax1994@gmail.com>
2024-06-21 18:47:51 +0100

    more UI improvements - now it looks great!

--- PairPods/LicenseManager.swift
+++ PairPods/LicenseManager.swift
@@ -48,16 +48,18 @@ struct LicenseManager: View {
 
     var body: some View {
         VStack {
-            Text("Welcome to PairPods!")
-                .font(.largeTitle)
-                .padding()
-            
-            Text("""
-                 You are welcome to use PairPods Free for as long as you like, but the audio sharing feature is limited to 5 minutes at a time.
-                 Alternatively, you can try our 7-day trial version for unlimited audio sharing, or purchase a lifetime Pro version license.
-                 """)
-                .padding()
-                .fixedSize(horizontal: false, vertical: true)
+            VStack(spacing: 4) {
+                Text("Welcome to PairPods!")
+                    .font(.largeTitle)
+                    .bold()
+                    .padding(.top)
+                
+                Text("Please select your desired license type.")
+                    .font(.title2)
+                    .foregroundColor(.secondary)
+                    .fixedSize(horizontal: false, vertical: true)
+            }
+            .padding(.bottom, 20)
             
             if purchaseManager.products.isEmpty {
                 Text("Loading products...")
@@ -65,7 +67,7 @@ struct LicenseManager: View {
             } else {
                 VStack(spacing: 16) {
                     productSelectionView(
-                        title: "Continue using for free",
+                        title: "PairPods Free",
                         description: "Use for free for as long as you like. Audio sharing limited to 5 minutes at a time.",
                         price: "",
                         isSelected: selectedOption == .free,
@@ -98,12 +100,13 @@ struct LicenseManager: View {
             }) {
                 Text("Continue")
                     .bold()
-                    .padding()
                     .frame(maxWidth: .infinity)
+                    .padding()
                     .background(Color.blue)
                     .foregroundColor(.white)
                     .cornerRadius(10)
             }
+            .buttonStyle(PlainButtonStyle()) // Ensure custom button style is applied
             .padding(.horizontal)
             
             Button(action: {
@@ -111,16 +114,17 @@ struct LicenseManager: View {
             }) {
                 Text("Restore Purchases")
                     .bold()
-                    .padding()
                     .frame(maxWidth: .infinity)
+                    .padding()
                     .background(Color.gray)
                     .foregroundColor(.white)
                     .cornerRadius(10)
             }
+            .buttonStyle(PlainButtonStyle()) // Ensure custom button style is applied
             .padding(.horizontal)
         }
         .padding()
-        .frame(minWidth: 400, minHeight: 300) // Minimum window size
+        .frame(minWidth: 400, minHeight: 550) // Minimum window size
         .alert(isPresented: $showAlert) {
             Alert(title: Text("Purchase Error"), message: Text(alertMessage), dismissButton: .default(Text("OK")))
         }
@@ -148,6 +152,7 @@ struct LicenseManager: View {
                         .stroke(isSelected ? Color.blue : Color.gray, lineWidth: 2)
                         .background(isSelected ? Color.blue.opacity(0.1) : Color.clear)
                         .cornerRadius(10))
+        .contentShape(Rectangle()) // Make entire area tappable
         .onTapGesture {
             selectionAction()
         }
